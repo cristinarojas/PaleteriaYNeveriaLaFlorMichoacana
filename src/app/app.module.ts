ac890dfc51077ff079395910282d0079fd4ce114
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-08-03 14:54:56 -0700

    Adding routing code for the project
    Adding the Angular route for the project and also the different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
+// Dependencies
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from "@angular/router";
 
+// Components
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { LocationsComponent } from './locations/locations.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    CarouselComponent,
+    HomeComponent,
+    ProductsComponent,
+    GalleryComponent,
+    LocationsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'productos', component: ProductsComponent },
+      { path: 'galeria', component: GalleryComponent },
+      { path: 'ubicaciones', component: LocationsComponent },
+      { path: 'contacto', component: ContactComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
