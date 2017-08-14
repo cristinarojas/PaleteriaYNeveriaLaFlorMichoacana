bc26186e2a07c4a0f0b1f56a6580c5b516dc5cc2
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-08-14 13:53:34 -0500

    Adding the special product section on home section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import homeData from '../../assets/data/homeData';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public homeData;
 
   constructor() { }
 
   ngOnInit() {
+    this.homeData = homeData;
   }
 
 }
---
+++ src/assets/data/homeData.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    image: 'nachos.png',
+    title: 'nachos',
+    descriptionone: 'Deliciosos nachos con queso amarillo',
+    descriptiontwo: 'Preparalos a tu gusto'
+  }
+];
